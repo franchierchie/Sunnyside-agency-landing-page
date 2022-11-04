9ab2ac6f22ca9a3cf64e2b30a268b8dee1a48094
Franco Chierchie <franchierchie@gmail.com>
2022-11-04 14:37:29 -0300

    Second upload

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const hamburgerMenu = document.querySelector('.navigation-container-header');
 const hamburger = document.querySelector('.hamburger-menu-container');
+const arrowObj = document.querySelector('.arrow');
 
 hamburger.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('hidden');
+});
+
+window.addEventListener('load', () => {
+    let timerId = null;
+    let y = 0;
+    
+    timerId = setInterval(arrow, 5);
+
+    function arrow() {
+        if (y >= 370) {
+            clearInterval(timerId);
+        } else {
+            y+=5;
+            arrowObj.style.top = y + 'px';
+        }
+    }
 });
